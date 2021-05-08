c590cfd9a225b75e588b6e263a46caa67fd9e663
Dukemz <>
2021-05-08 16:32:57 +0000

    i have no idea what the heck i'm doing

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
+// Just some testing for something I'm going to make in the future. - Dukemz
+
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const readline = require('readline');
+// const io = require("socket.io")(http);
 
-app.use(express.static("public"));
+require('dotenv').config();
+const config = require('./config.json');
+// serve images, CSS files, and JavaScript files in a directory named public:
+var test = app.use(express.static("public"));
 
 app.get('*', function(req, res) {
   const ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.ip;
-  console.log(`GET request to [${req.url}] from ${ip}`)
-  // if(req.url.includes('package') || req.url.includes('node_modules')) return res.status().send("haha no.");
-  if(config.forbidden.some(v => req.url.includes(v))) return res.sendFile(__dirname + "error.html?err=403");
-  res.sendFile(__dirname + req.url);
+  console.log(`GET request to [${req.url}] from ${ip}`);
+  // if(config.forbidden.some(v => req.url.includes(v))) {
+  //   console.log(403)
+  //   return res.status(403).redirect("/error.html?err=403");
+  // }
+  // if(!fs.existsSync(req.url)) {
+  //   console.log(404)
+  //   return res.status(404).redirect("/error.html?err=404");
+  // }
+  // res.sendFile(__dirname + req.url);
+
 });
 
-http.listen(3000, () => {
-  console.log("Server online - listening on port 3000.");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+rl.on('line', function(line) {
+  try {
+    console.log(eval(line));
+  } catch(err) {
+    console.error(err);
+  }
+});
+
+http.listen(config.port, () => {
+  console.log(`Server online - listening on port ${config.port}.`);
 });
